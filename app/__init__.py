7ed942390b2da792a1ededce76cb85ff29642df0
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-06 01:35:26 +0300

    creates a file macros.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
+from distutils import extension
 from flask import Flask
 from .config import DevConfig
-from flask_sqlalchemy import SQLAlchemy
- 
-app = Flask(__name__)
-# Setting up configuration
-app.config.from_object(DevConfig)
-
 from app import views
-db = SQLAlchemy()
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+def create_app(config_name):
+    app = Flask(__name__)
+    # Setting up configuration
+    app.config.from_object(DevConfig)
+
+    bootstrap = Bootstrap()
+    db = SQLAlchemy()
+
+    # initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
